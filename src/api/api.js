20b629a11c3129ef126aa3c0fae7dedfd1f83949
mbrad26 <mbrad26@gmail.com>
2020-09-05 15:28:31 +0100

    add input form

---
+++ src/api/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const axiosGitHubGraphQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  },
+});
--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,27 @@ import React from 'react';
 
 const TITLE = 'React GraphQL GitHub Client';
 
-const App = () =>
-  <div>
-    <h1>{TITLE}</h1>
-  </div>
+const App = () => {
+
+  const handleSubmit = () => null;
+  const handleChange = () => null;
+
+  return (
+    <div>
+      <h1>{TITLE}</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='url'>
+          Show open issues for https://github.com/
+        </label>
+        <input
+          id='url'
+          type='text'
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  );
+};
+
 
 export default App;
